6403e8b822c70d22a3e85677b500890d9809bfd7
PoritskiyW <91318154+PoritskiyW@users.noreply.github.com>
2021-12-21 18:08:34 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    let html = '';
+
+    switch (req.url) {
+
+        case '/':
+            html = 'home.html';
+            break;
+
+        case '/questions':
+            html = 'questions.html';
+            break;
+
+        case '/about':
+            html = 'about.html';
+            break;
+
+        default:
+            break;
+    }
+
+    if (html) {
+
+        fs.stat(`./${html}`, (err, stats) => {
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+
+            if (stats) {
+
+                fs.createReadStream(html).pipe(res);
+            } else {
+
+                res.statusCode = 404;
+                res.end('Page not found');
+            }
+        })
+    }
+})
+
+server.listen(3000, 'localhost', () => {
+
+    console.log('server is on')
+})
+
