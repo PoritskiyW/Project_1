12d1eaa63f9bff48929b29d67d76fa9e9adb87ed
viktoria <vikaberezuk12345@gmail.com>
2021-12-29 00:04:54 +0200

    add server home page

--- server/src/modules/dataFill.js
+++ server/src/modules/dataFill.js
@@ -3,39 +3,98 @@ const fs = require('fs');
 function createDataDir() {
 
     fs.mkdir('./data', (e) => {
-        if(!e || (e && e.code !== 'EEXIST')){
+        if (!e || (e && e.code !== 'EEXIST')) {
 
-            fs.writeFile('./data/developers.json', `{"developers": [
-            ]}`, (err) => {
-                if(err) {
+            fs.writeFile('./data/developers.json', `{
+  "person": [
+    {
+      "id": 1,
+      "name": "Viacheslav",
+      "surname": "",
+      "age": 23,
+      "sex": "Male",
+      "birthday": "October 29, 1998",
+      "locations": "Kharkov",
+      "hobby": "music, anime, hiking",
+      "images": "masyanya-wait.gif"
+    },
+    {
+      "id": 2,
+      "name": "Viktoria",
+      "surname": "Bereziuk",
+      "age": 26,
+      "sex": "Female",
+      "birthday": "July 20, 1995",
+      "locations": "Kharkov",
+      "hobby": "Travel, gum, cinema, hiking, music",
+      "images": "masyanya-wait.gif"
+    },
+    {
+      "id": 3,
+      "name": "Yehor",
+      "surname": "",
+      "age": 18,
+      "sex": "Male",
+      "birthday": "July 24, 2003",
+      "locations": "Kharkov",
+      "hobby": "games, music, reading, technologies",
+      "images": "masyanya-wait.gif"
+    },
+    {
+      "id": 4,
+      "name": "Vladyslav",
+      "surname": "",
+      "age": 21,
+      "sex": "Male",
+      "birthday": "May 2, 2000",
+      "locations": "Kharkov",
+      "hobby": "MMO games",
+      "images": "masyanya-wait.gif"
+    }
+  ]
+}`, (err) => {
+                if (err) {
                     throw err;
                 }
             });
             fs.writeFile('./data/questions.xml', `<?xml version="1.0" encoding="UTF-8" ?>
 <root></root>`, (err) => {
-                if(err) {
+                if (err) {
                     throw err;
                 }
             });
             fs.writeFile('./data/questions.yaml', `questions: `, (err) => {
-                if(err) {
+                if (err) {
                     throw err;
                 }
             });
             fs.writeFile('./data/questions.csv', '', (err) => {
-                if(err) {
+                if (err) {
                     throw err;
                 }
             });
-            fs.writeFile('./data/questions.json', `{"questions": [
-            ]}`, (err) => {
-                if(err) {
+            fs.writeFile('./data/questions.json', `{
+  "questions": [
+    {
+      "id": 1,
+      "questionAsk": "questionAsk1",
+      "theme": "theme1",
+      "answer": "true",
+      "fileSystem": [
+        "JSON",
+        "CSV",
+        "YAML",
+        "XML"
+      ],
+      "dateModify": "18.12.2021"
+    }]}`, (err) => {
+                if (err) {
                     throw err;
                 }
             });
             fs.writeFile('./data/fileSystemThemes.json', `{"fileSystems": [
             ]}`, (err) => {
-                if(err) {
+                if (err) {
                     throw err;
                 }
             });
--- server/src/server.js
+++ server/src/server.js
@@ -2,68 +2,71 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dataFill = require('./modules/dataFill');
-//let question = require('./questions.json');
+//const question = require('./../data/questions.json');
+//const person = require('./../data/developers.json');
 //TODO: Переделать чтение и запись файлов на папку дата + убрать require на файле
-const parse = require('./modules/questionData')
+//const parse = require('./modules/questionData')
 
 const server = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-
-/*
 //'Access-Control-Allow-Origin'
 server.use(cors);
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.sendFile(path.resolve(path.resolve(), 'static', './../../web/dist/views/home.html'));
     res.status(200);
 });
 
-server.get('/download', (req, res) => {
-    res.download(path.resolve(__dirname, 'static', './../../web/dist/views/home.html'));
-    res.status(200);
-});
-
-//question
-//handle get request for questions.html
-server.get('/questions', (request, response) => {
-    response.json(question.questions);
-    console.log('request questions');
-})
-server.use(express.json());
-//handle post request to add question
-server.post('/addQuestion', (request, response) => {
-    const addQuestion = {
-        id: question.questions.length + 1,
-        questionAsk: request.body.questionAsk,
-        theme: request.body.theme,
-        answer: request.body.boolean,
-        fileSystem: request.body.fileSystem,
-        dateModify: new Date() //Дату добавлять на фронте и передавать в JSON на бэк
-    };
-    question.questions.push(addQuestion);
-    console.log(request.body);
-    return response.json(addQuestion);
-});
-
-//handle delete question data
-server.delete('/deleteQuestion', (request, response) => {
-    const index = question.questions.findIndex(el => el.id);
-    if (index >= 0) {
-        const el = question.questions[index];
-        question.questions.splice(index, 1);
-        response.json(el);
-        response.end();
-    } else {
-        response.status(404);
-        response.end();
-    }
-})*/
+// server.get('/download', (req, res) => {
+//     res.download(path.resolve(__dirname, 'static', './../../web/dist/views/home.html'));
+//     res.status(200);
+// });
+//home
+// server.get('/home', (req, res) => {
+//     res.json(person.person);
+//     console.log('request person');
+// })
+// //question
+// //handle get request for questions.html
+// server.get('/questions', (request, response) => {
+//     response.json(question.questions);
+//     console.log('request questions');
+// })
+//
+// //handle post request to add question
+// server.post('/addQuestion', (request, response) => {
+//     const addQuestion = {
+//         id: question.questions.length + 1,
+//         questionAsk: request.body.questionAsk,
+//         theme: request.body.theme,
+//         answer: request.body.boolean,
+//         fileSystem: request.body.fileSystem,
+//         dateModify: new Date() //Дату добавлять на фронте и передавать в JSON на бэк
+//     };
+//     question.questions.push(addQuestion);
+//     console.log(request.body);
+//     return response.json(addQuestion);
+// });
+//
+// //handle delete question data
+// server.delete('/deleteQuestion', (request, response) => {
+//     const index = question.questions.findIndex(el => el.id);
+//     if (index >= 0) {
+//         const el = question.questions[index];
+//         question.questions.splice(index, 1);
+//         response.json(el);
+//         response.end();
+//     } else {
+//         response.status(404);
+//         response.end();
+//     }
+// })
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    dataFill();
+   // dataFill();
 });
 
