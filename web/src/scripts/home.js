ae1063b3773bc1e39395f3770a186beea1ac78d0
PoritskiyW <PoritskiyW@gmail.com>
2022-01-12 16:29:51 +0300

    bug fixes and serializers updae

--- web/src/scripts/home.js
+++ web/src/scripts/home.js
@@ -52,20 +52,28 @@ function modalUser(devData) {
 
     for (let i = 0; i < devData.length; i++) {
         const item = devData[i];
-        const mySection = document.createElement('section');
-        mySection.id = `user${item.id}`;
-        mySection.innerHTML = `
+        const form = document.createElement('form');
+        form.id = `user${item.id}`;
+        form.name = `user`
+        form.enctype = 'multipart/form-data';
+        form.method = 'post'
+            form.innerHTML = `
             <div class="img__content">
                 <img src="${item.images}" alt='person'/>
+                <input type="file" name="AddImage" accept="image/*" >
             </div>
-            <label>Name: <input type='text'  maxlength="20" placeholder=${item.name}></label>
-            <label>Surname: <input type='text'  maxlength="20" placeholder=${item.surname}></label>
-            <label>Gender: <input type='text' maxlength="20" placeholder=${item.sex}></label>
-            <label>Age: <input type='number'  min="0" max="100" placeholder=${item.age}></label>
-            <label>Birthday: <input type="date"  value='${item.birthday}'></label>
-            <label>Locations: <input type='text'  maxlength="20" placeholder=${item.locations}></label>
-            <label>Hobby: <input type='text' maxlength="100"  placeholder=${item.hobby}></label>`;
+            <label>Name: <input type='text'  maxlength="20" placeholder=${item.name} name="Name"></label>
+            <label>Surname: <input type='text'  maxlength="20" placeholder=${item.surname} name="Surname"></label>
+            <label>Gender: <input type='text' maxlength="20" placeholder=${item.sex} name="Gender"></label>
+            <label>Age: <input type='number'  min="0" max="100" placeholder=${item.age} name="Age"></label>
+            <label>Birthday: <input type="date"  value='${item.birthday}' name="Birthday"></label>
+            <label>Locations: <input type='text'  maxlength="20" placeholder=${item.locations} name="Locations"></label>
+            <label>Hobby: <input type='text' maxlength="100"  placeholder=${item.hobby} name="Hobby"></label>
+            <div class="form-developer__button">
+                <button class="button" id="saveDeveloper${item.id}" type="submit">save</button>
+                <button class="button" id="cancelDeveloper${item.id}" type="reset">cancel</button>
+            </div>`
 
-        infoDeveloper.appendChild(mySection);
+        infoDeveloper.appendChild(form);
     }
 }
